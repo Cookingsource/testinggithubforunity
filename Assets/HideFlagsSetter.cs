10ee9dad19b46526a87d3d7f76c3121108e2da72
Rafael Muñoz León <rafaelmunozleon@Rafaels-MacBook-Pro.local>
2018-09-11 09:55:29 +0200

    First commit

---
+++ Assets/HideFlagsSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class HideFlagsSetter : MonoBehaviour 
+{
+	public HideFlags flags;
+
+	public HideFlags currentFlags;
+
+	// Use this for initialization
+	[ContextMenu("Set Flags")]
+	void Set () 
+	{
+		this.gameObject.hideFlags = flags;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		currentFlags = this.gameObject.hideFlags;
+	}
+}
